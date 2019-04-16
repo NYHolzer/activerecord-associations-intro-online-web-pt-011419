70681b9ab6259eaddcf8d2084361fcd3abf11412
Nissan Holzer <nissan.holzer@gmail.com>
2019-04-16 11:56:46 +0000

    Automatically backed up by Learn

--- db/migrate/02_create_genres.rb
+++ db/migrate/02_create_genres.rb
@@ -1,7 +1,11 @@
 class CreateGenres < ActiveRecord::Migration[4.2]
   def change
+<<<<<<< HEAD
     create_table :genres do |t|
       t.string :name
     end 
+=======
+    t.string :name
+>>>>>>> 9d54ccde6613844e33f27dc96750e67b7eda0f31
   end
 end
